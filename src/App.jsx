fae2adf61ab371c5c461b77da9c1795d3968467e
pawara101 <pawara101dassanayake@gmail.com>
2024-06-25 09:41:49 +0530

    Coding exercise 12. Dynamic list content

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 
-// IMPORTANT:
-// In this Udemy environment, you CAN'T import & use useState like this:
-// import { useState } from 'react'
-// Instead, import & use it like this:
-// import React from 'react';
-// React.useState(...)
+import Todo from './Todo'
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
 
 // don't change the Component name "App"
-
-
 export default function App() {
-  const [selectedProceed, setSelectedProceed] = React.useState();
-    //button click function
-    const handleProceed = (selectedButton) => {
-      setSelectedProceed(selectedButton)
-      console.log("selectedButton");
-      
-    }
   
-    return (
-      <div>
-        <div data-testid="alert" id="alert">
-          <h2>Are you sure?</h2>
-          <p>These changes can't be reverted!</p>
-          <button onClick={handleProceed}>Proceed</button>
-        </div>
-        <button>Delete</button>
-      </div>    
-    );
 }
