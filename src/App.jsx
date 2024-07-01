dccb0703f099ea8c8061f1d4ba042e35cc16eef6
pawara101 <pawara101dassanayake@gmail.com>
2024-07-02 01:02:48 +0530

    Section 04 | exercise 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-
-import Todo from './Todo'
-
-// don't remove the export keyword here!
-export const DUMMY_TODOS = [
-    'Learn React',
-    'Practice React',
-    'Profit!'
-];
-
-// don't change the Component name "App"
-export default function App() {
-  return (
-      <ul>
-          {DUMMY_TODOS.map((item) => (
-              <Todo>{...item}</Todo>
-          ))}
-      </ul>
-  );
-}
+function Summary({ text }) {
+    return (<>
+    <h1>Summary</h1>
+    <p>{text}</p>
+    </>);
+  }
+  
+  function App() {
+    return (
+      <div id="app" data-testid="app">
+        <Summary text="Fragments help you avoid unnecessary HTML elements." />
+    
+      </div>
+    );
+  }
+  
+  export default App;
